4ac03607b79b5af69bb0e5ee77d98b454e517ec4
uxbenkel <uxbenkel@github.com>
2023-11-09 07:29:13 +0800

    增加日志时间戳

--- update.sh
+++ update.sh
@@ -9,6 +9,7 @@ qb_web_port='8080'
 qb_username='admin'
 qb_password='adminadmin'
 qb_ip='localhost'
+now_time=$(date +%Y-%m-%d)' '$(date +%H:%M:%S)
 
 # wrap text info
 text="<b>natmap 打洞成功通知</b>
@@ -20,13 +21,13 @@ bot_api='bot_api'
 chat_id='chat_id'
 url="https://api.telegram.org/bot${bot_api}/sendMessage"
 
-echo "Update qBittorrent listen port to $public_port..."
+echo "[$now_time] Update qBittorrent listen port to $public_port..."
 
 # Update qBittorrent listen port.
 qb_cookie=$(curl -s -i --header "Referer: http://$qb_ip:$qb_web_port" --data "username=$qb_username&password=$qb_password" http://$qb_ip:$qb_web_port/api/v2/auth/login | grep -i set-cookie | cut -c13-48)
 curl -X POST -b "$qb_cookie" -d 'json={"listen_port":"'"$public_port"'"}' "http://$qb_ip:$qb_web_port/api/v2/app/setPreferences"
 
-echo "Update iptables..."
+echo "[$now_time] Update iptables..."
 
 # Use iptables to forward traffic.
 LINE_NUMBER=$(iptables -t nat -nvL --line-number | grep "$private_port" | head -n 1 | awk '{print $1}')
@@ -35,9 +36,9 @@ if [ "$LINE_NUMBER" != "" ]; then
 fi
 iptables -t nat -I PREROUTING -p tcp --dport "$private_port" -j REDIRECT --to-port "$public_port"
 
-echo "Done."
+echo "[$now_time] Done."
 
 #发送消息
 curl -s -X POST ${url} -d chat_id=${chat_id} -d text="${text}" -d parse_mode=HTML >/dev/null 2>&1
 
-echo "Message has been sent."
+echo "[$now_time] Message has been sent."
