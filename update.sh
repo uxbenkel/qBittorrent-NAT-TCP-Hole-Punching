ffc829b276bc13a22655a97ec2e1191e50bad1c6
uxbenkel <uxbenkel@github.com>
2023-11-09 07:27:09 +0800

    新增日志时间戳

--- update.sh
+++ update.sh
@@ -5,23 +5,25 @@ private_port=$4 # Natter: $3; NATMap: $4
 public_port=$2 # Natter: $5; NATMap: $2
 
 # qBittorrent.
-qb_web_port="8080"
-qb_username="admin"
-qb_password="adminadmin"
+qb_web_port='8080'
+qb_username='admin'
+qb_password='adminadmin'
+qb_ip='localhost'
+now_time=$(date +%Y-%m-%d)' '$(date +%H:%M:%S)
 
-echo "Update qBittorrent listen port to $public_port..."
+echo "[$now_time] Update qBittorrent listen port to $public_port..."
 
 # Update qBittorrent listen port.
-qb_cookie=$(curl -s -i --header "Referer: http://localhost:$qb_web_port" --data "username=$qb_username&password=$qb_password" http://localhost:$qb_web_port/api/v2/auth/login | grep -i set-cookie | cut -c13-48)
-curl -X POST -b "$qb_cookie" -d 'json={"listen_port":"'$public_port'"}' "http://localhost:$qb_web_port/api/v2/app/setPreferences"
+qb_cookie=$(curl -s -i --header "Referer: http://$qb_ip:$qb_web_port" --data "username=$qb_username&password=$qb_password" http://$qb_ip:$qb_web_port/api/v2/auth/login | grep -i set-cookie | cut -c13-48)
+curl -X POST -b "$qb_cookie" -d 'json={"listen_port":"'"$public_port"'"}' "http://$qb_ip:$qb_web_port/api/v2/app/setPreferences"
 
-echo "Update iptables..."
+echo "[$now_time] Update iptables..."
 
 # Use iptables to forward traffic.
-LINE_NUMBER=$(iptables -t nat -nvL --line-number | grep ${private_port} | head -n 1 | awk '{print $1}')
-if [ "${LINE_NUMBER}" != "" ]; then
-    iptables -t nat -D PREROUTING $LINE_NUMBER
+LINE_NUMBER=$(iptables -t nat -nvL --line-number | grep "$private_port" | head -n 1 | awk '{print $1}')
+if [ "$LINE_NUMBER" != "" ]; then
+    iptables -t nat -D PREROUTING "$LINE_NUMBER"
 fi
-iptables -t nat -I PREROUTING -p tcp --dport $private_port -j REDIRECT --to-port $public_port
+iptables -t nat -I PREROUTING -p tcp --dport "$private_port" -j REDIRECT --to-port "$public_port"
 
-echo "Done."
+echo "[$now_time] Done."
